6ad4e0556b7b81e8bb13a2adac370b969e9a760b
Tochi Onwuasoanya <tochiboy1@gmail.com>
2023-06-02 23:50:10 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,30 +1,32 @@
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
+
 data = {
     "asin": "TESTING123D",
     "title": "Mark 1adsf"
 }
 
+class Product(Model):
+    """
+    Represents a product in the 'scraper_app' keyspace.
+    """
 
-# List View -> Detail View
-class Product(Model): # -> table
-    __keyspace__ = "scraper_app" # Keyspace from astradb database
+    __keyspace__ = "scraper_app"
     asin = columns.Text(primary_key=True, required=True)
     title = columns.Text()
     brand = columns.Text()
     price_str = columns.Text(default="-100")
     country_of_origin = columns.Text()
 
-# Detail View for asin
-class ProductScrapeEvent(Model): # -> table
-    __keyspace__ = "scraper_app" #
-    uuid = columns.UUID(primary_key=True) # uuid.uuid1() -> #time
+class ProductScrapeEvent(Model):
+    """
+    Represents a scrape event for a product in the 'scraper_app' keyspace.
+    """
+
+    __keyspace__ = "scraper_app"
+    uuid = columns.UUID(primary_key=True)
     asin = columns.Text(index=True)
     title = columns.Text()
     brand = columns.Text()
-    country_of_origin = columns.Text() 
+    country_of_origin = columns.Text()
     price_str = columns.Text(default="-100")
-
-# def this -> ProductScrapeEvent.objects().filter(asin="AMZNIDNUMBER")
-
-# not this -> Product.objects().filter(title="Mark 1")
