cf2ee66333d88a44204dd686b6837df62b660d12
Tochi Onwuasoanya <tochiboy1@gmail.com>
2023-06-02 23:53:25 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,10 @@
 # Convert Cassandra UUID Field to Pydantic Datetime Str 
 import datetime
 
-def uuid1_time_to_datetime(time:int):
+def uuid1_time_to_datetime(time: int):
     """
-    Start datetime is on October 15th, 1582. 
-    WHY? https://en.wikipedia.org/wiki/1582
-    
-    add the time from uuid.uui1().time 
-    divided by 10 (ignoring the remainder thus //)
+    Converts a time value from a UUID1 field in Cassandra to a Python datetime object.
+    The start datetime is set to October 15th, 1582.
+    The time value is added to the start datetime, divided by 10 (ignoring the remainder).
     """
-    return datetime.datetime(1582, 10, 15) + datetime.timedelta(microseconds=time//10)
+    return datetime.datetime(1582, 10, 15) + datetime.timedelta(microseconds=time // 10)
