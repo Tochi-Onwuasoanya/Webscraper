2adc78df5eb89d737b435dd35b7fb695e3cf8d86
Tochi Onwuasoanya <tochiboy1@gmail.com>
2023-06-02 23:47:28 -0400

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,11 @@ from functools import lru_cache
 
 from pydantic import BaseSettings, Field
 
+# Set environment variable if not already set
 if os.getenv("CQLENG_ALLOW_SCHEMA_MANAGEMENT") is None:
     os.environ["CQLENG_ALLOW_SCHEMA_MANAGEMENT"] = "1"
 
+# Define settings using Pydantic
 class Settings(BaseSettings):
     name: str = Field(..., env="PROJ_NAME")
     db_client_id: str = Field(..., env='ASTRA_DB_CLIENT_ID')
@@ -17,7 +19,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
 
-
+# Cache the settings to avoid repeated calculations
 @lru_cache
 def get_settings():
     return Settings()
