6981ce64c95670d820517d02c5c4d5c6ebb2cbcc
Tochi Onwuasoanya <tochiboy1@gmail.com>
2023-06-02 23:52:44 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,20 +3,24 @@
 import time
 from dataclasses import dataclass
 from fake_useragent import UserAgent
-
 import re
 from requests_html import HTML
 from slugify import slugify
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 def get_user_agent():
+    """
+    Generates a random user agent string.
+    """
     return UserAgent(verify_ssl=False).random
 
 
 def extract_price_from_string(value: str, regex=r"[\$]{1}[\d,]+\.?\d{0,2}"):
+    """
+    Extracts the price value from a string using regular expression matching.
+    """
     x = re.findall(regex, value)
     val = None
     if len(x) == 1:
@@ -26,20 +30,31 @@ def extract_price_from_string(value: str, regex=r"[\$]{1}[\d,]+\.?\d{0,2}"):
 
 @dataclass
 class Scraper:
+    """
+    Web scraper class for extracting data from a web page using Selenium and Requests-HTML.
+    """
+
     url: str = None
     asin: str = None
-    endless_scroll : bool = False
+    endless_scroll: bool = False
     endless_scroll_time: int = 5
     driver: WebDriver = None
     html_obj: HTML = None
 
     def __post_init__(self):
+        """
+        Initializes the Scraper instance by setting the URL based on the ASIN value.
+        Raises an exception if ASIN or URL is missing.
+        """
         if self.asin:
             self.url = f"https://www.amazon.com/dp/{self.asin}/"
         if not self.asin or not self.url:
-            raise Exception(f"asin or url is required.")
+            raise Exception("ASIN or URL is required.")
 
     def get_driver(self):
+        """
+        Initializes and returns the Selenium WebDriver.
+        """
         if self.driver is None:
             user_agent = get_user_agent()
             options = Options()
@@ -51,10 +66,12 @@ class Scraper:
         return self.driver
 
     def perform_endless_scroll(self, driver=None):
+        """
+        Performs endless scrolling on the web page to load dynamically generated content.
+        """
         if driver is None:
             return
         if self.endless_scroll:
-            # driver.execute_script
             current_height = driver.execute_script("return document.body.scrollHeight")
             while True:
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
@@ -66,6 +83,9 @@ class Scraper:
         return 
 
     def extract_element_text(self, element_id):
+        """
+        Extracts the text content of an element identified by its ID.
+        """
         html_obj = self.get_html_obj()
         el =  html_obj.find(element_id, first=True)
         if not el:
@@ -73,10 +93,16 @@ class Scraper:
         return el.text
     
     def extract_tables(self):
+        """
+        Extracts all table elements from the web page.
+        """
         html_obj = self.get_html_obj()
         return html_obj.find("table")
     
     def extract_table_dataset(self, tables) -> dict:
+        """
+        Extracts a dataset from the table elements on the web page.
+        """
         dataset = {}
         for table in tables:
             for tbody in table.element.getchildren():
@@ -96,7 +122,6 @@ class Scraper:
                     key = row[0]
                     value = row[1]
                     
-                    # print(key, value)
                     data = {}
                     key = slugify(key)
                     key = key.replace("-", "_")
@@ -115,12 +140,18 @@ class Scraper:
         return dataset
 
     def get_html_obj(self):
+        """
+        Initializes and returns the Requests-HTML HTML object.
+        """
         if self.html_obj is None:
             html_str = self.get()
             self.html_obj = HTML(html=html_str)
         return self.html_obj 
 
     def get(self):
+        """
+        Retrieves the web page using Selenium WebDriver and returns the page source.
+        """
         driver = self.get_driver()
         driver.get(self.url)
         if self.endless_scroll:
@@ -130,6 +161,9 @@ class Scraper:
         return driver.page_source
 
     def scrape(self):
+        """
+        Performs the scraping operation by extracting data from the web page.
+        """
         html_obj = self.get_html_obj()
         price_str = self.extract_element_text('#priceblock_ourprice')
         title_str = self.extract_element_text('#productTitle')
